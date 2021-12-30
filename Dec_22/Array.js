d27a73933c48db37e1913fc86beba08162ce6f2f
shaikh_md.007 <94097592+shaikhmd007@users.noreply.github.com>
2021-12-30 18:57:12 +0530

    Add files via upload

---
+++ Dec_22/Array.js
@@ -0,0 +1,35 @@
+console.log("Array Programs: ");
+var Arr=[123,234,56,78,910,1,4];
+console.log(Arr);
+console.log(typeof Arr);
+
+//Read
+var b=[1,2,"L","M",0,true,"True"];
+console.log(b);
+console.log(Arr[3]);
+console.log(b[3]);
+
+//Update:
+console.log("[0]");
+b[0]="ONE As String";
+console.log(b[0]);
+
+b[3][0]="T"
+console.log(b[3][0]);
+
+// Check The length of an Array:
+
+console.log(Arr.length);
+
+// Array push:
+
+Arr.push("Dynamically Added A String..");
+console.log(Arr);
+//Check Pushed Array ->>
+console.log(Arr.length);
+
+Arr.pop();
+console.log(Arr.length);
+
+
+var aaa[]; aaa[10]
